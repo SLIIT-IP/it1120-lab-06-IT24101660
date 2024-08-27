2662f76e05681c26459940af350f0b09217f2dbc
IT24101660 <IT24101660@my.sliit.lk>
2024-08-27 20:12:51 +0530

    IT24101660Lab6Q2 to Q2

---
+++ Q2/IT24101660Lab6Q2.java
@@ -0,0 +1,11 @@
+public class IT24101660Lab6Q2{
+	public static void main(String[]args){
+		
+		int count = 1;
+		while (count<=10)
+		{
+		System.out.print(count+" ");
+		count=count+1;
+		}
+	}
+}
